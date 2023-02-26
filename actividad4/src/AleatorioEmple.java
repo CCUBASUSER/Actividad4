1dda3a6e75a65f9129b54bec8ba9ad7b5b9406cf
Carlos Cubas <116658635+CCUBASUSER@users.noreply.github.com>
2023-02-26 18:38:38 +0100

    Imnprimimos nombre del metoodo

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -5,6 +5,7 @@ import java.io.RandomAccessFile;
 public class AleatorioEmple {
 
 	public static void main(String[] args) throws IOException {
+		System.out.println("METODO MAIN");
 		crear();
 		leer();
 	}
@@ -40,7 +41,7 @@ public class AleatorioEmple {
 			file.close(); 
 			System.out.println(" ------------------------------------------");
 		} else 
-			System.out.println(" ---------FICHERO VACÍO --------------------");
+			System.out.println(" ---------FICHERO VACï¿½O --------------------");
 	}// fin verporconsola
 
 	public static void crear() throws IOException {
